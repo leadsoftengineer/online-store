4b1f4ba116fd15e4ae799c62f851f1fca290f6cf
smolinks <smolinks.com@hotmail.com>
2019-07-08 19:00:38 +0300

    Updates

---
+++ precache-manifest.d6bf2ae874b92f4e13f505c03867dc8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d596fda48c2baf59f1a0b09ceb0a73d4",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "33d1b99748ee2cfadbbb",
+    "url": "/online-store/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "be87bcc839d93c2b7919",
+    "url": "/online-store/static/css/main.ce986a27.chunk.css"
+  },
+  {
+    "revision": "33d1b99748ee2cfadbbb",
+    "url": "/online-store/static/js/2.d15e18b0.chunk.js"
+  },
+  {
+    "revision": "be87bcc839d93c2b7919",
+    "url": "/online-store/static/js/main.4778aee0.chunk.js"
+  },
+  {
+    "revision": "19e0f7521b8bac23d01b",
+    "url": "/online-store/static/js/runtime~main.ca6cc33d.js"
+  },
+  {
+    "revision": "4d4323968785d693710ee7785a05f6e3",
+    "url": "/online-store/static/media/tel.4d432396.svg"
+  }
+]);
